f5caadd1aa1908254f1192026e6495179c402260
Phutawan Palakavong <88098103+pphtw@users.noreply.github.com>
2025-05-02 11:41:47 +0700

    Update app_KPI_64130500066.py

--- app_KPI_64130500066.py
+++ app_KPI_64130500066.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import pickle
 
 # Load model and encoders
